7267344be29bcb8f2fe5b6d3b4143574180a8eb9
kriskova <kriskovamail@gmail.com>
2014-08-05 09:08:53 +0200

    Hamming exercise updated | More ruby-ish solution.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,10 +1,5 @@
 class Hamming
   def self.compute(a, b)
-    hamming_number = 0
-    min_length = [a.length,b.length].min
-    (0..min_length-1).each do |i|
-      hamming_number += 1 unless a[i] == b[i]
-    end
-    hamming_number
+    [a.length,b.length].min.times.count{|i| a[i] != b[i]} 
   end
 end
