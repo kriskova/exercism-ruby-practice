7985aa55d2c6f2b4d377c45b1db7521ec102d547
kriskova <kriskovamail@gmail.com>
2014-08-13 09:59:19 +0200

    Raindrops exercise | 3rd iteration

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -3,9 +3,8 @@ class Raindrops
   def self.convert(number)
     found = "" 
     convert = {3 => "Pling", 5 => "Plang", 7 => "Plong"}
-    convert.default=""
     factors = Prime.prime_division(number)
-    factors.each { |factor,_| found << convert[factor] }
+    factors.each { |factor,_| found << convert.fetch(factor,"") }
     found.empty? ? number.to_s : found
   end
 end
