1f8a7bb41850d85cfb43a5d45d50a37efeb69d91
kriskova <kriskovamail@gmail.com>
2014-08-28 12:55:40 +0200

    Phone number exercise | 2nd iteration

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -2,20 +2,25 @@ class PhoneNumber
   attr_reader :area_code, :number
 
   def initialize(phone_number)
-    @number = cleanup_number(phone_number)
-    @area_code = @number.chars.take(3).join
+    @raw_number = phone_number
+    @number = verified_raw_number
+    @area_code = @number[0..2]
   end
   
   def to_s
-    "(%s) %s-%s" % [@number[0,3], @number[3,3], @number[6..-1]]
+    "(%s) %s-%s" % [@area_code, @number[3,3], @number[6..-1]]
   end
 
   private
 
-  def cleanup_number(number)
-    clean = number.scan(/^\D?(\d{3})\D?\s?(\d{3})\D?(\d{4,5})$/).join
-    return "0000000000" unless clean =~ /^1?\d{10}$/
+  def verified_raw_number
+    return "0000000000" unless valid_number?
 
-    clean.length == 11 ? clean[1..-1] : clean
+    @raw_number.length == 11 ? @raw_number[1..-1] : @raw_number
+  end
+
+  def valid_number?
+    @raw_number = @raw_number.scan(/^\D?(\d{3})\D?\s?(\d{3})\D?(\d{4,5})$/).join
+    @raw_number =~ /^1?\d{10}$/
   end
 end
