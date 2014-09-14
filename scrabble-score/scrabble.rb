0abe826a49dc32256af5b18d684053bd6551bb7e
kriskova <kriskovamail@gmail.com>
2014-09-14 14:31:18 +0200

    Scrabble exercise | 2nd iteration

--- scrabble-score/scrabble.rb
+++ scrabble-score/scrabble.rb
@@ -1,6 +1,14 @@
 class Scrabble
 
-  LETTER_VALUES = {}
+  LETTER_VALUES = {
+    1 => ["A","E","I","O","U","L","N","R","S","T"],
+    2 => ["D","G"],
+    3 => ["B","C","M","P"],
+    4 => ["F","H","V","W","Y"],
+    5 => ["K"],
+    8 => ["J","X"],
+    10 => ["Q","Z"]
+  }
 
   def self.score(word)
     Scrabble.new(word).score
@@ -8,30 +16,11 @@ class Scrabble
 
   def initialize(word)
     @word = word.nil? ? "" : word.gsub(/[^a-zA-Z]/,"")
-    generate_letter_values()
   end
 
   def score
-    @word.chars.inject(0){|sum, char| sum += LETTER_VALUES[char.upcase]}
-  end
-
-  private
-
-  def generate_letter_values
-    raw = [
-      [1,"A","E","I","O","U","L","N","R","S","T"],
-      [2,"D","G"],
-      [3,"B","C","M","P"],
-      [4,"F","H","V","W","Y"],
-      [5,"K"],
-      [8,"J","X"],
-      [10,"Q","Z"]
-    ]
-    raw.each do |serie|
-      serie.each_with_index do |char, index|
-        LETTER_VALUES[char] = serie[0] unless index == 0
-      end
+    LETTER_VALUES.inject(0) do |sum, (score, letters)|
+      sum + score * @word.chars.count {|i| letters.include?(@word[i].upcase)}
     end
   end
-
 end
