8cf8480ba55090f86beffffb5787d06304348586
kriskova <kriskovamail@gmail.com>
2014-09-24 00:12:11 +0200

    Secret handshake exercise | refactor HANDSHAKES to GESTURES

--- secret-handshake/secret_handshake.rb
+++ secret-handshake/secret_handshake.rb
@@ -1,6 +1,6 @@
 class SecretHandshake
   
-  HANDSHAKES = ["wink","double blink","close your eyes","jump"]
+  GESTURES = ["wink","double blink","close your eyes","jump"]
 
   def initialize(number)
     number = 0 unless number.kind_of?(Integer)
@@ -9,7 +9,7 @@ class SecretHandshake
 
   def commands
     result = []
-    HANDSHAKES.each_with_index do |item, index|
+    GESTURES.each_with_index do |item, index|
       result << item if @number.chars.reverse[index] == "1"
     end
     @number.size == 5 && @number[0] == "1" ? result.reverse : result
