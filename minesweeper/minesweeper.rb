b562a5bdf880558f2dc166ffbc32bdbc2d80eb96
kriskova <kriskovamail@gmail.com>
2014-10-14 22:34:41 +0200

    Minesweeper | Not finished, but have to sleep

---
+++ minesweeper/minesweeper.rb
@@ -0,0 +1,17 @@
+class Board
+
+  def self.transform(board)
+    board.each_with_index do |row, r|
+      row.each_with_index do |item, i|
+        #TODO finish
+      end
+    end
+  end
+
+  private
+
+  def self.count_neighbors(board, row, pos)
+
+  end
+
+end
