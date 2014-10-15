ae9e28652f129c17f14fcafc7135021d35a36ca6
kriskova <kriskovamail@gmail.com>
2014-10-15 23:54:22 +0200

    Minesweeper | Still not functioning, but closer..no time again

--- minesweeper/minesweeper.rb
+++ minesweeper/minesweeper.rb
@@ -2,8 +2,8 @@ class Board
 
   def self.transform(board)
     board.each_with_index do |row, r|
-      row.each_with_index do |item, i|
-        #TODO finish
+      row.chars.each_with_index do |item, i|
+        board[r][i] = count_neighbors(board, r, i) if item == " "
       end
     end
   end
@@ -11,7 +11,20 @@ class Board
   private
 
   def self.count_neighbors(board, row, pos)
-
+    counter = 0
+    if row > 0
+      counter += 1 if board[row - 1][pos] == "*"
+    end
+    if row < board.size - 1
+      counter += 1 if board[row + 1][pos] == "*"
+    end
+    if pos > 0
+      counter += 1 if board[row][pos - 1] == "*"
+    end
+    if pos < board[row].length - 1
+      counter += 1 if board[row][pos + 1] == "*"
+    end
+    counter.to_s
   end
 
 end
