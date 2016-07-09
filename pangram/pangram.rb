9ddb4e8af8d658cf9dcc3d28d5ac134c607c8b15
kriskova <kriskovamail@gmail.com>
2016-07-09 15:37:41 +0200

    Pangram | Minor fix

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -3,15 +3,12 @@ module BookKeeping
 end
 
 class Pangram
-  include BookKeeping
 
   ALPHABET = ('a'..'z').to_a
 
   def self.is_pangram?(sentence)
     (ALPHABET - chars_in_sentence(sentence.downcase)).empty?
   end
-
-  private
 
   def self.chars_in_sentence(sentence)
     sentence.scan(/\w/)
