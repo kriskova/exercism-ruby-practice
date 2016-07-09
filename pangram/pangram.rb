9f232e7a5356dff6cece83ddfdf0e9027e06eb68
kriskova <kriskovamail@gmail.com>
2016-07-09 17:18:40 +0200

    Pangram | Facepalm

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -4,15 +4,7 @@ end
 
 class Pangram
 
-  ALPHABET = ('a'..'z').to_a
-
   def self.is_pangram?(sentence)
-    (ALPHABET - chars_in_sentence(sentence.downcase)).empty?
+    ('a'..'z').all? { |char| sentence.downcase.include? char }
   end
-
-  def self.chars_in_sentence(sentence)
-    sentence.scan(/\w/)
-  end
-  private_class_method :chars_in_sentence
-
 end
