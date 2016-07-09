941ac085d72b198a15cecbf698ce3a9f519aed8d
kriskova <kriskovamail@gmail.com>
2016-07-09 15:53:23 +0200

    RunLengthEncoding | Refactor decode

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -13,19 +13,11 @@ class RunLengthEncoding
   end
 
   def self.decode(text)
-    text
-      .scan(/\d*\D{1}/)
-      .map { |chunk| decode_chunk(chunk) }
-      .join
+    text.gsub(/\d+./) { |str| str[-1] * str.to_i }
   end
 
   def self.encode_chunk(char, chunk)
     chunk_counter = chunk.size > 1 ? chunk.size.to_s : ''
     chunk_counter + char
-  end
-
-  def self.decode_chunk(chunk)
-    chunk_size = chunk.to_i
-    chunk_size.zero? ? chunk.chars.last : chunk.chars.last * chunk_size
   end
 end
